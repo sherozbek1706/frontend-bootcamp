edb44ef10651040341679c93834d6c0b10ab4e9a
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2022-08-15 12:42:19 +0500

    hozircha html keyinroq reactda quyaman

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let hamburger = document.querySelector(".icon-ham");
+let icon_x = document.querySelector(".icon-x");
+let nav = document.querySelector(".nav");
+
+hamburger.addEventListener('click' , ()=> {
+  nav.classList.toggle('click')
+})
+icon_x.addEventListener('click' , ()=> {
+  nav.classList.toggle('click')
+})
+
